3f84ca547c1fd807dc2b08efadfacb8185a69083
Shawna Staff <staffs@my.lanecc.edu>
2022-09-19 08:15:26 -0700

    commit

--- graphs/graph.js
+++ graphs/graph.js
@@ -1,13 +1,48 @@
-// edge list
-const graphE = [[0, 2], [2, 3], [2, 1], [1, 3]];
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
 
-//adjacent list
-const graphA = [[2], [2, 3], [0, 1, 3], [1, 2]];
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+  addEdge(node1, node2) {
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1)
+   }
 
-//adjacent matrix 
-const graph = [
-  [0, 0, 1, 0],
-  [0, 0, 1, 1],
-  [1, 1, 0, 1],
-  [0, 1, 1, 0]
-];
+  showConnections() {
+    let allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+}
+
+let graph = new Graph();
+graph.addVertex('0');
+graph.addVertex('1');
+graph.addVertex('2');
+graph.addVertex('3');
+graph.addVertex('4');
+graph.addVertex('5');
+graph.addVertex('6');
+
+graph.addEdge("3", "1");
+graph.addEdge("3", "4");
+graph.addEdge("4", "2");
+graph.addEdge("4", "5");
+graph.addEdge("1", "2");
+graph.addEdge("1", "0");
+graph.addEdge("0", "2");
+graph.addEdge("6", "5");
+
+graph.showConnections();
