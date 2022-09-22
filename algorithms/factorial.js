27f9dc9ed447a93f441d0e39ace5699604e81e9b
Shawna Staff <staffs@my.lanecc.edu>
2022-09-22 11:27:51 -0700

    typo

--- algorithms/factorial.js
+++ algorithms/factorial.js
@@ -3,7 +3,7 @@
 function findFactorialRecursive(number) {
   // base case that stops the recursive calls
   if (number === 2) {
-    //when 2 is returned the previous recursive calls then run, popping off the stack, returning the decremeted value from each call and multiplying it be the next call in the stack.
+    //when 2 is returned the previous recursive calls then run, popping off the stack, returning the decremeted value from each call and multiplying it by the next call in the stack.
     return 2;
   }
   // no "real" processing takes places we are just decrementing the number with each call
